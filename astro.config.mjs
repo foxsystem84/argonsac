90d95715c60f4ac604777c4f049a352add557988
Grover LLanos <gllanosh@gmail.com>
2024-12-06 09:56:38 -0500

    Proyecto segunda revision

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt()],
+  output: "static",
+  adapter: vercel(),
 });
